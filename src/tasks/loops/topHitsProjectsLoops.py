449e9a0abde82a056dbba9dc68334acd4f2c6c8f
aya <ayam57752@gmail.com>
2026-05-08 01:08:14 +0300

    Merge branch 'main' of https://github.com/MZiyad24/wikimedia-spark

--- src/tasks/loops/topHitsProjectsLoops.py
+++ src/tasks/loops/topHitsProjectsLoops.py
@@ -1,21 +1,28 @@
 def run(rdd):
     data = rdd.collect()
 
+    # dictionary 3shan n store total hits l kol project
     project_hits = {}
 
     for row in data:
+
+        # extract project code and hits
         project = row["project"]
         hits = row["hits"]
 
+        # law el project msh mawgood addo b 0
         if project not in project_hits:
             project_hits[project] = 0
 
+        # ngeb total hits l kol project
         project_hits[project] += hits
 
+    # sort descending by total hits
     sorted_projects = sorted(
         project_hits.items(),
         key=lambda x: x[1],
         reverse=True
     )
 
+    # return top 5 projects
     return sorted_projects[:5]
--- src/tasks/mapreduce/topHitsProjectsReduce.py
+++ src/tasks/mapreduce/topHitsProjectsReduce.py
@@ -1,9 +1,14 @@
 def run(rdd):
     return (
         rdd
+        # remove invalid rows
         .filter(lambda x: x.get("project") and x.get("hits"))
+        # transform row -> (project, hits)
         .map(lambda x: (x["project"], int(x["hits"])))
+        # combine hits for same project
         .reduceByKey(lambda a, b: a + b)
-        .map(lambda x: (x[1], x[0]))
-        .takeOrdered(5, key=lambda x: -x[0])
+        #(project, total_hits)
+        .map(lambda x: (x[0], x[1]))
+        # get top 5 descending by hits
+        .takeOrdered(5, key=lambda x: -x[1])
     )
